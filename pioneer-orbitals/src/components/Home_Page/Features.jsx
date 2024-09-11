58d417ce3f06021fd0f573e99c518371492d5b43
aryamanrtunjay <aryamanrtunjay@gmail.com>
2024-09-10 23:58:35 -0700

    Almost done with features

--- pioneer-orbitals/src/components/Home_Page/Features.jsx
+++ pioneer-orbitals/src/components/Home_Page/Features.jsx
@@ -2,10 +2,41 @@
 
 import React from 'react'
 import AccordionItem from './AccordionItem'
+import Button from '../Button';
+import FeaturesImage from '@/images/FeaturesImage.png'
+import Image from 'next/image'
 
 export default function Features() {
 
     const [open, setOpen] = React.useState(false);
+    const [isDesktop, setIsDesktop] = React.useState(true);
+
+    const checkWindowSize = () => {
+        let windowWidth;
+        if(typeof window !== "undefined") {
+        windowWidth = window.innerWidth
+        }
+
+        if (windowWidth > 1024) {
+        setIsDesktop(true)
+        } else {
+        setIsDesktop(false)
+        }
+    }
+
+    React.useEffect(() => {
+        function handleResize() {
+        checkWindowSize();
+        }
+        window.addEventListener('resize', handleResize);
+        return () => {
+        window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    React.useEffect(() => {
+        checkWindowSize()
+    }, [])
 
     const toggle = (index) => {
         if(open === index) {
@@ -34,12 +65,23 @@ export default function Features() {
     ]
 
     return (
-        <section className="h-screen grid place-items-center">
-            <div className="px-[40px] max-w-[800px]">
-                {accordionData.map((data, index) => {
-                    return <AccordionItem key={index} open={index === open} title={data.title} content={data.content} toggle={() => toggle(index)}/>
-                })}
-            </div>
-        </section>
+        <div className="flex items-center mx-[50px] mb-[100px] justify-center gap-10">
+            {isDesktop && <div className="rounded-[50px]">
+                <Image 
+                    src={FeaturesImage} 
+                    alt="Image of Rocket Launch" 
+                    className="h-auto object-cover rounded-[70px]"
+                />
+            </div>}
+            <section className="h-full flex flex-col justify-center items-center gap-5">
+                <h1 className="text-[40px] lg:text-[80px] text-center">Why Choose Us?</h1>
+                <div className="px-[40px] max-w-[800px] mb-5">
+                    {accordionData.map((data, index) => {
+                        return <AccordionItem key={index} open={index === open} title={data.title} content={data.content} toggle={() => toggle(index)}/>
+                    })}
+                </div>
+                <Button href="/#bookings" variant="outline" className="h-15 w-full max-w-[720px] text-xl text-center mt-5">Reserve your Flight</Button>
+            </section>
+        </div>
     )
 }
